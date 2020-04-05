99a215b3a06cdcc4d8e5490dd83da87bf6cdea07
robert.king@radiancetech.com <kingbob2015@hotmail.com>
2020-04-05 17:08:14 -0400

    Started mailing list project

---
+++ apis/MailingList/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res) {
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
+
+  console.log(`name is ${firstName} ${lastName} with an email of ${email}`);
+});
+
+app.listen(3000, function() {
+  console.log("Server is running on port 3000");
+});
