e83d0ac3ea9381a663a8bb52b8fdf0526f2dc7b8
robert.king@radiancetech.com <kingbob2015@hotmail.com>
2020-04-05 18:35:03 -0400

    finish mailing list project with files and changes needed to deploy to heroku

--- apis/MailingList/app.js
+++ apis/MailingList/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
 
 const app = express();
 
@@ -16,9 +17,49 @@ app.post("/", function(req, res) {
   const lastName = req.body.lastName;
   const email = req.body.email;
 
-  console.log(`name is ${firstName} ${lastName} with an email of ${email}`);
+  const mailchimpApiKey = "fb84ce57c57b5c46e7ea052154f724c9-us19";
+  const mailchimpListId = "e73d196e3a";
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ],
+    update_existing: true
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = `https://us19.api.mailchimp.com/3.0/lists/${mailchimpListId}`;
+  const options = {
+    method: "POST",
+    auth: `foo:${mailchimpApiKey}`
+  };
+  const request = https.request(url, options, response => {
+    response.on("data", data => {
+      const code = response.statusCode;
+      if (code === 200) {
+        res.sendFile(__dirname + "/success.html");
+      } else {
+        res.sendFile(__dirname + "/failure.html");
+      }
+      console.log(JSON.parse(data));
+    });
+  });
+  request.write(jsonData);
+  request.end();
 });
 
-app.listen(3000, function() {
-  console.log("Server is running on port 3000");
+app.post("/failure", function(req, res) {
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server is running");
 });
