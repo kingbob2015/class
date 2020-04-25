0c18b7c12d4677996b4f51d2af89a1f5032ef6f9
robert.king@radiancetech.com <kingbob2015@hotmail.com>
2020-04-25 15:13:00 -0400

    bcrypt and salting

--- secrets/app.js
+++ secrets/app.js
@@ -3,7 +3,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
 const app = express();
 
 app.use(express.static("public"));
@@ -31,15 +33,17 @@ app.route("/login")
     })
     .post((req, res) => {
         const username = req.body.username;
-        const password = md5(req.body.password);
+        const password = req.body.password;
         User.findOne({ email: username }, (err, foundUser) => {
             if (err) {
                 console.log(err);
             } else {
                 if (foundUser) {
-                    if (password === foundUser.password) {
-                        res.render("secrets");
-                    }
+                    bcrypt.compare(password, foundUser.password, (err, result) => {
+                        if (result) {
+                            res.render("secrets");
+                        }
+                    })
                 }
             }
         })
@@ -50,17 +54,19 @@ app.route("/register")
         res.render("register");
     })
     .post((req, res) => {
-        const newUser = new User({
-            email: req.body.username,
-            password: md5(req.body.password)
+        bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+            const newUser = new User({
+                email: req.body.username,
+                password: hash
+            })
+            newUser.save((err) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    res.render("secrets")
+                }
+            });
         })
-        newUser.save((err) => {
-            if (err) {
-                console.log(err);
-            } else {
-                res.render("secrets")
-            }
-        });
     })
 
 app.listen(3000, () => {
